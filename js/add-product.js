0fb80004fb2f0b076a94925294056dce9b94ac03
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-02-12 10:51:34 +0300

    Collect form data into a product object

--- js/add-product.js
+++ js/add-product.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
    const form = document.getElementById("productform");
    if (!form) return;
+     form.addEventListener("submit", function (e) {
+       e.preventDefault();
