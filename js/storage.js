aefb83789a3c421d63cdac5bf7c9a0453c2d5157
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-02-12 11:11:21 +0300

    add getProducts + saveProducts

--- js/storage.js
+++
@@ -1,7 +0,0 @@
-function getProducts() {
-  return JSON.parse(localStorage.getItem("products")) || [];
-}
-
-function saveProducts(products) {
-  localStorage.setItem("products", JSON.stringify(products));
-}
