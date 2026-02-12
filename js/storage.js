ec6d523a26f4e38380c90a2370b1cd34ebc3bb77
Cecilia <cessmorah42@gmail.com>
2026-02-12 09:22:16 +0300

    added storage.js which is a  Central file for handling localStorage logic and change script.js to dashboard.js

---
+++ js/storage.js
@@ -0,0 +1,10 @@
+// storage.js
+// Central file for handling localStorage logic
+
+function getProducts() {
+  return JSON.parse(localStorage.getItem("products")) || [];
+}
+
+function saveProducts(products) {
+  localStorage.setItem("products", JSON.stringify(products));
+}
